bdb9f9a8fb368258c1d0df793f51577e5726c9a6
lastro <1279349Qwerty>
2021-06-05 15:08:11 +0300

    add change for success deploy to tomcat

--- src/main/java/com/example/onlyreciver/OnlyReciverApplication.java
+++ src/main/java/com/example/onlyreciver/OnlyReciverApplication.java
@@ -2,12 +2,20 @@ package com.example.onlyreciver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class OnlyReciverApplication {
-
+public class OnlyReciverApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(OnlyReciverApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(OnlyReciverApplication.class);
+    }
 }
+
+
